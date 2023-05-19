576cb0fb66de3542b9a38d89382f2fc9facf91eb
faiz khan <kfaiz2215@gmail.com>
2023-05-19 16:15:38 +0530

    Custom server added

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
-const express = require('express');
+const { createServer } = require('http');
+const { parse } = require('url');
 const next = require('next');
-
+ 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const hostname = 'localhost';
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
-
+ 
 app.prepare().then(() => {
-  const server = express();
-
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
-  server.all('/api/*', (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(process.env.PORT || 1429, (err) => {
-    if (err) throw err;
-    console.log('Server started on port 1429');
-  });
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+ 
+      if (pathname === '/a') {
+        await app.render(req, res, '/a', query);
+      } else if (pathname === '/b') {
+        await app.render(req, res, '/b', query);
+      } else {
+        await handle(req, res, parsedUrl);
+      }
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  })
+    .once('error', (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
 });
